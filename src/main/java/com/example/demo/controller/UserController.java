b36b463c8b8c0c3ba5478409ad7157bfb03e44fb
moquz <1203277194@qq.com>
2021-07-09 09:08:10 +0800

    springboot

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,12 +4,9 @@ import com.example.demo.bean.Result;
 import com.example.demo.bean.Test;
 import com.example.demo.bean.User;
 import com.example.demo.mapper.TestMapper;
-import com.example.demo.service.impl.PageServiceImpl;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +18,7 @@ import java.util.List;
 public class UserController {
     @Autowired
     private TestMapper testMapper;
-    @Autowired
-    private PageServiceImpl pageService;
-
+   
     @RequestMapping("/findAll")
     public List<Test>findAll(){
         return testMapper.finAll();
@@ -48,8 +43,8 @@ public class UserController {
         User user=new User(1,"张三","12345","男");
         return new Result<>(user);
     }
-    @GetMapping("/page")
+   /* @GetMapping("/page")
     public PageInfo SelectPage( com.example.demo.bean.PageInfo pageInfo){
         return pageService.findByIdPage(pageInfo);
-    }
+    }*/
 }
--- src/main/java/com/example/demo/service/PageService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.github.pagehelper.PageInfo;
-
-public interface PageService {
-
-    PageInfo findByIdPage(com.example.demo.bean.PageInfo pageInfo);
-
-}
